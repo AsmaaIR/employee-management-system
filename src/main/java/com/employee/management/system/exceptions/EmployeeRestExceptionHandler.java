c9993d6341221bbed7074a4e776cd98d5e04ee2d
AsmaaIR <asmaaibrahim06@gmail.com>
2021-01-17 11:51:31 +0200

    rename file

--- src/main/java/com/employee/management/system/exceptions/EmployeeRestExceptionHandler.java
+++ src/main/java/com/employee/management/system/exceptions/EmployeeRestExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class StudentRestExceptionHandler {
+public class EmployeeRestExceptionHandler {
 
 
 	// Add an exception handler using @ExceptionHandler
